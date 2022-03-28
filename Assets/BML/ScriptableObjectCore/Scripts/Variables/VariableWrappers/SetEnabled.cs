ec79bf52288834ebcb69a4f98f53eabf4da24bf4
Dan C <djc5627@gmail.com>
2022-03-28 10:36:03 -0400

    Initial Commit

---
+++ Assets/BML/ScriptableObjectCore/Scripts/Variables/VariableWrappers/SetEnabled.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BML.ScriptableObjectCore.Scripts.Variables;
+using Sirenix.Utilities;
+using UnityEngine;
+
+namespace BML.ScriptableObjectCore.Scripts.Variables.VariableWrappers
+{
+    public class SetEnabled : MonoBehaviour
+    {
+        [SerializeField] private BoolReference Enabled;
+        [SerializeField] private MonoBehaviour[] Targets;
+
+        private void Start()
+        {
+            Enabled.Subscribe(() =>
+            {
+                Targets.ForEach(t => t.enabled = Enabled.Value);
+            });
+        }
+    }
+}
